e1c9b0bd23e2380337b193863ef61f584786881d
ViewShumoon <ViewShumoon@outlook.com>
2024-06-13 21:26:30 +0800

    更新 模板

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,32 +7,93 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="container">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="@assets/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="@assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+      starter
     </p>
-  </div>
+    <p>
+      Install
+      <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+      in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
 </template>
 
 <style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 .read-the-docs {
   color: #888;
+}
+
+button {
+  color: #fff;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+
+button:hover {
+  border-color: #646cff;
+}
+
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
+.card {
+  padding: 2em;
+}
+
+
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+
+a:hover {
+  color: #535bf2;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,95 @@
-import { defineConfig } from 'vite'
+import path from "path";
+
+import { defineConfig, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((({ mode }) => {
+    const envConfig = loadEnv(mode, process.cwd());
+
+    const VITE_PORT = Number(envConfig.VITE_PORT);
+    const VITE_API_URL = envConfig.VITE_API_URL;
+
+    return {
+        base: "./",
+        server: {
+            open: false,
+            port: VITE_PORT,
+            host: "0.0.0.0",
+            //host: "localhost",
+            cors: true,
+            proxy: {
+                "/api": {
+                    target: VITE_API_URL,
+                    //target: "http://localhost:6030/",
+                    ws: true,
+                    secure: false,
+                    changeOrigin: true, // 是否允许跨域代理
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+        plugins: [
+            vue(),
+            AutoImport({
+                imports: [
+                    "vue",
+                    // {
+                    //     "naive-ui": [
+                    //         "useDialog",
+                    //         //"useMessage",
+                    //         //"useNotification",
+                    //         //"useLoadingBar",
+                    //     ],
+                    // },
+                ],
+                resolvers: [NaiveUiResolver()],
+                dts: path.resolve(resolve("src/types/auto-imports.d.ts")),
+            }),
+            Components({
+                extensions: ["vue", "md"],
+                dirs: ["src/components", "src/views", "src/layouts"],
+                resolvers: [NaiveUiResolver()],
+                dts: "src/types/components.d.ts",
+                include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+                types: [],
+            }),
+        ],
+        build: {
+            sourcemap: true,
+        },
+        resolve: {
+            alias: {
+                "@": resolve("./src"),
+                "@assets": resolve("./src/assets"),
+
+                "@types": resolve("./src/types"),
+                "@components": resolve("./src/components"),
+                "@views": resolve("./src/views"),
+                '@router': resolve('./src/router'),
+                
+                "@api": resolve("./src/api"),
+                "@stores": resolve("./src/stores"),
+
+                '@utils': resolve('./src/utils')
+            },
+            extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+            define: {
+                __APP_ENV__: envConfig.APP_ENV,
+                __VUE_I18N_FULL_INSTALL__: true,
+                __VUE_I18N_LEGACY_API__: false,
+                __INTLIFY_PROD_DEVTOOLS__: false,
+            },
+        },
+    };
+
+}));
+
+function resolve(p: string) {
+    return path.resolve(__dirname, p);
+}
